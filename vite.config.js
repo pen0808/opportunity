92d61d95b5dc442065b6b40e61dca58e94958fe4
Paul Nwaoduah <nwaoduahpaul@gmail.com>
2026-09-07 17:08:22 +0100

    configure vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/opportunity/',
+  base: process.env.VITE_BASE_PATH || '/opportunity/',
   plugins: [react()],
 })
